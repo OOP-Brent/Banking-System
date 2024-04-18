1366ac66cfa4166f556832f92b1c757da538075f
OOP-Brent <166787728+OOP-Brent@users.noreply.github.com>
2024-04-19 00:18:37 +0800

    Add files via upload
    For OOP ITE232
    Working Bank System by Brent Bernardo
    
    When Logging in
    Bank Acc ID : 27200409
    Pin: 1927

---
+++ BankingSystemGUI/src/BankingSystem/bank.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package BankingSystem;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Administrator
+ */
+public class bank {
+
+    private static final bank bankO = new bank();
+
+    private double balance = 1000;
+    ArrayList<String> transactions = new ArrayList<>(20);
+
+    public static bank getInstance() {
+        return bankO;
+
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    public void deposite(double balance, int added) {
+        this.balance = balance + added;
+        transactions.add("you added " + added + " to your balance \n");
+
+    }
+
+    private bank() {
+    }
+
+    public void withdrawl(double balance, int number) {
+        if (balance > 0) {
+            double newbalance = balance - number;
+            transactions.add(" you withdrawed " + number + " from your balance \n");
+            setBalance(newbalance);
+        }
+    }
+}
